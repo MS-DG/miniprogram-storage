e183cedcc426fb6efcbf46aad14f286b839e54ab
New Future (Weilai LIU) <New.Future@microsoft.com>
2019-04-02 17:42:58 +0800

    feat(API): add removeStorage & clearStorage

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@
  */
 
 /**
- * 配置项
+ * 全局配置项
  */
 export const config = {
     /**
@@ -22,10 +22,10 @@ export const config = {
 };
 
 /**
- * 读取storage
+ * 异步读取storage
  * @param key 键
  * @template TResult 返回值类型
- * @template key 键值类型
+ * @template Tkey 键值类型
  */
 export function getStorage<TResult = unknown, TKey = string>(key: TKey): Promise<TResult> {
     return new Promise<TResult>((resolve, reject) => {
@@ -43,13 +43,16 @@ export function getStorage<TResult = unknown, TKey = string>(key: TKey): Promise
 }
 
 /**
- * 写入取storage
+ * 异步写入取storage
  * @param key 键
  * @param data 值
  * @template TResult 返回值类型
- * @template key 键值类型
+ * @template Tkey 键值类型
  */
-export function setStorage<TData = unknown, TKey = string>(key: TKey, data: TData): Promise<wx.GeneralCallbackResult> {
+export function setStorage<TData = unknown, TKey = string>(
+    key: TKey,
+    data: TData,
+): Promise<wx.GeneralCallbackResult> {
     return new Promise<wx.GeneralCallbackResult>((resolve, reject) => {
         wx.setStorage({
             data,
@@ -63,12 +66,48 @@ export function setStorage<TData = unknown, TKey = string>(key: TKey, data: TDat
     });
 }
 
+/**
+ * 异步删除storage
+ * @param key 键
+ * @template Tkey 键值类型
+ */
+export function removeStorage<Tkey extends string = string>(
+    key: Tkey,
+): Promise<wx.GeneralCallbackResult> {
+    return new Promise<wx.GeneralCallbackResult>((resolve, reject) => {
+        wx.removeStorage({
+            fail: (res: any) => {
+                config.logger && config.logger.error(`wx.setStorage:${key}`, res);
+                reject(res);
+            },
+            key,
+            success: resolve,
+        });
+    });
+}
+/**
+ * 异步清空storage
+ */
+export function clearStorage(): Promise<wx.GeneralCallbackResult> {
+    return new Promise<wx.GeneralCallbackResult>((resolve, reject) => {
+        wx.clearStorage({
+            fail: (res: any) => {
+                config.logger && config.logger.error("wx.clearStorage:", res);
+                reject(res);
+            },
+            success: resolve,
+        });
+    });
+}
+
 declare const console: any;
 // tslint:disable-next-line: no-namespace
 declare namespace wx {
-    export function getStorage(op: any): void;
-    export function setStorage(op: any): void;
-    export interface GeneralCallbackResult {
+    function getStorage(op: any): void;
+    function setStorage(op: any): void;
+    function removeStorage(op: any): void;
+    function clearStorage(op: any): void;
+    interface GeneralCallbackResult {
         errMsg: string;
     }
 }
